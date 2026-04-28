e62db141c12f37cab17c155059424a5bf80a29db
Kunapureddy Sri Sai Vasist <workvasist@gmail.com>
2026-04-28 21:58:53 +0530

    Changed main.py

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ from evaluate import evaluate_model
 from compress import compress_model
 
 if __name__ == "__main__":
-    # print("STEP 1: Training model...")
-    # train_model()
+    print("STEP 1: Training model...")
+    train_model()
 
-    # print("\nSTEP 2: Evaluating model...")
-    # evaluate_model()
+    print("\nSTEP 2: Evaluating model...")
+    evaluate_model()
 
     print("\nSTEP 3: Applying compression...")
     compress_model()
